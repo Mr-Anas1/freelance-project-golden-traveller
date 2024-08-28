959844a3e177f21b281ca289dcfe05eb92c64b33
Mohitpuri Goswami <mohitgoswami379@gmail.com>
2024-08-28 22:58:01 +0530

    customer addition

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import image from '../images/image-removebg-preview.png';
+
 import logo1 from '../images/logo1.jpg';
 import logo2 from '../images/logo2.jpg';
 import logo3 from '../images/logo3.jpg';
 import logo4 from '../images/logo4.png';
 import logo5 from '../images/logo5.jpg';
+import logo6 from "../images/logo 6.jpeg"
+import logo7 from "../images/logo 7.jpeg"
+import logo8 from "../images/logo 8.jpeg"
+import logo9 from "../images/logo 9.jpeg"
+import logo10 from "../images/logo 10.jpeg"
+import logo11 from "../images/logo 11.jpeg"
+import logo12 from "../images/logo 12.jpeg"
+import logo13 from "../images/logo 13.jpeg"
 
 function Customers() {
-	const logos = [logo1, logo2, logo3, logo4, logo5];
+	const logos = [logo1, logo2, logo3, logo4, logo5,logo6,logo7,logo8,logo9,logo10,logo11,logo12,logo13];
 	return (
 		<>
 			<div
